d7a67a6afe009da629202cb5b929b03864129993
theboyGolden <preciousstick15@gmail.com>
2024-05-18 13:55:57 +0000

    .

--- callback.js
+++ callback.js
@@ -3,15 +3,20 @@
 
 //example below
 
-const buy = () => {
+const buy = (pay_callback) => {
     console.log("buying a car")
+    pay_callback()
 }
 
 const pay = () => {
     console.log("paying for the car")
 }
 
-buy ()
-pay ()
+buy (pay)
+
 
 //the functions are working correctly
+//now to the callback function
+
+//it is evident that the pay function has been passed as an argument to the buy function 
+//the code therefore executes 'buying a car' and then 'paying for the car' due to the callback
